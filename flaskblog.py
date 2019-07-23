30eab15bbc8bd03719aa6213d57106994e6589f3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-07-23 14:24:54 -0300

    Dummy data

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Myke Albuquerque Pinto de Oliveira',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'April 20, 2019'
+	},
+	{
+		'author': 'Maria Caliente de las Dores',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'June 21, 2019'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 3',
+		'content': 'Third post content',
+		'date_posted': 'July 18, 2019'
+	}
+]
 
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 def home():
-	return '<h1>Home Page</h1>'
+	return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-	return 'About'
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
