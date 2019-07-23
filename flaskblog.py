080e6e5050c670e687a3281c367f2fede5c7a887
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-07-23 15:14:33 -0300

    Styles made

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -31,7 +31,7 @@ def home():
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+	return render_template('about.html', title='About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
